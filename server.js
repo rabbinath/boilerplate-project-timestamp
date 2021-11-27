2789c925e8654a38588d0d01a4892dbb496e7449
Rabindra Nath <rabbinath@gmail.com>
2021-11-27 08:08:09 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
+const { response } = require('express');
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
@@ -24,9 +25,19 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+let resObj={};
+app.get("/api/:inputDate?",function(req,res)  {
+let inputDate=req.params.inputDate;
+if(inputDate.includes('-')){
+  resObj['unix']=new Date(inputDate).getTime();
+  res.json(resObj);
+}
+  
+});
 
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
+
